4be0bfe8ffc07c201dec12a68d6d05656433874a
malleswar <malleswar.gangarapu@gmail.com>
2026-05-19 19:37:10 +0530

    encode file updated

---
+++ encode.py
@@ -0,0 +1,15 @@
+def encode(msg):
+    if not msg:
+        return """"""
+    result = []
+    count = 1
+    for i in range(1,len(msg)):
+        if msg[i]==msg[i-1]:
+            count += 1
+        else:
+            result.append( msg[i-1]+str(count))
+            count = 1
+
+    result.append(msg[i-1]+str(count))
+    return "".join(result)
+print(encode("AAAABBCCDAA"))
