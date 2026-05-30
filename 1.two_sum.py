133582e5d01962e387321f905086c73ebaba1781
malleswar0511 <malleswar.gangarapu@gmail.com>
2026-05-30 10:06:17 +0530

    new commit

--- 1.two_sum.py
+++ 1.two_sum.py
@@ -1,13 +1,13 @@
 def two_sum(nums,target):
     seen = {}
-    for i,value in enumerate(nums):
+    for _,value in enumerate(nums):
         compliment = target-value
         if compliment in seen:
-            return [seen[compliment],i]
-        seen[value] = i
+            return [seen[compliment],_]
+        seen[value] = _
         
         
         
-# print(two_sum([2,7,11,15], 9))  
+print(two_sum([2,7,11,15], 9))  
 print(two_sum([3,2,4], 6))      
-# print(two_sum([3,3], 6))
+print(two_sum([3,3], 6))
