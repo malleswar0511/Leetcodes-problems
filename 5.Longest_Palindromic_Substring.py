d3210a1e19f25ba8870fa77b73965a7eeb3f6b1b
malleswar0511 <malleswar.gangarapu@gmail.com>
2026-06-27 19:40:20 +0530

    longest_substring

---
+++ 5.Longest_Palindromic_Substring.py
@@ -0,0 +1,20 @@
+def check_polindrome(s):
+    return s == s[::-1]
+        
+    
+        
+    
+def longest_substring(n):
+    longest_substr = n[0]
+    for i in range(len(n)):
+        for j in range(i,len(n)):
+            sub_str = n[i:j+1]
+            if check_polindrome(sub_str) and len(sub_str)>len(longest_substr):
+                longest_substr = sub_str
+    return longest_substr
+            
+            
+n1= "babad"
+n2 = "cbbd"
+print(longest_substring(n1))
+print(longest_substring(n2))
